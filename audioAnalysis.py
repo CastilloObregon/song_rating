3af19392b767f66a1e785681b892e6035b4a452d
manupianista <manuelecastilloo@gmail.com>
2020-09-13 04:36:44 -0600

    kmeans clustering

--- audioAnalysis.py
+++ audioAnalysis.py
@@ -13,6 +13,7 @@ from numba import jit, cuda
 from numba import vectorize
 
 import pandas as pd
+import seaborn as sns
 
 from PIL import Image
 import pathlib
@@ -29,67 +30,179 @@ from sklearn.datasets import make_blobs
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 
-features = pd.read_csv(r'dataset.csv')
-features = pd.DataFrame(features)
-print(features)
+from sklearn.cluster import DBSCAN
+from sklearn.datasets import make_moons
+from sklearn.metrics import adjusted_rand_score
 
-# with open('dataset.csv', newline='') as f:
-#     reader = csv.reader(f)
-#     data = list(reader)
-
-# print(data)
-
-featuresFixed = features.iloc[:, 1:27]
-print(featuresFixed)
-
-scaler = StandardScaler()
-scaled_features = scaler.fit_transform(featuresFixed)
-# print(scaled_features)
-
-
-kmeans = KMeans(
-    init="random",
-    n_clusters=6,
-    n_init=10,
-    max_iter=300,
-    random_state=42
-)
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.metrics import silhouette_score, adjusted_rand_score
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 
 # ======= Ejecucion de Kmeans clustering ========
-kmeans.fit(scaled_features)
-
-print(kmeans.inertia_)
-print(kmeans.cluster_centers_)
-print(kmeans.n_iter_)
-
-print(kmeans.labels_)
 
 
+def kmeansBasico(the_features):
+    kmeans = KMeans(
+        init="random",
+        n_clusters=6,
+        n_init=10,
+        max_iter=300,
+        random_state=42
+    )
+    kmeans.fit(the_features)
+
+    print(kmeans.inertia_)
+    print(kmeans.cluster_centers_)
+    print(kmeans.n_iter_)
+
+    print(kmeans.labels_)
+
+    # Instantiate k-means and dbscan algorithms
+    kmeans = KMeans(n_clusters=2)
+    dbscan = DBSCAN(eps=0.3)
+
+    # Fit the algorithms to the features
+    kmeans.fit(the_features)
+    dbscan.fit(the_features)
+
+    # Compute the silhouette scores for each algorithm
+    kmeans_silhouette = silhouette_score(
+        scaled_features, kmeans.labels_).round(25)
+    dbscan_silhouette = silhouette_score(
+        scaled_features, dbscan.labels_).round(25)
+
+    kmeans_silhouette
+    dbscan_silhouette
+
+    # Plot the data and cluster silhouette comparison
+    fig, (ax1, ax2) = plt.subplots(
+        1, 2, figsize=(8, 6), sharex=True, sharey=True
+    )
+    fig.suptitle(f"Clustering Algorithm Comparison: Crescents", fontsize=16)
+    fte_colors = {
+        0: "#008fd5",
+        1: "#fc4f30",
+    }
+    # The k-means plot
+    km_colors = [fte_colors[label] for label in kmeans.labels_]
+    ax1.scatter(scaled_features[:, 0], scaled_features[:, 1], c=km_colors)
+    ax1.set_title(
+        f"k-means\nSilhouette: {kmeans_silhouette}", fontdict={"fontsize": 12}
+    )
+
+    # The dbscan plot
+    db_colors = [fte_colors[label] for label in dbscan.labels_]
+    ax2.scatter(scaled_features[:, 0], scaled_features[:, 1], c=db_colors)
+    ax2.set_title(
+        f"DBSCAN\nSilhouette: {dbscan_silhouette}", fontdict={"fontsize": 12}
+    )
+    plt.show()
 
 
+def kmeansElaborado(data, true_label_names):
+
+    print(data[:5, :3])
+    print(true_label_names[:5])
+    label_encoder = LabelEncoder()
+    true_labels = label_encoder.fit_transform(true_label_names)
+    true_labels[:5]
+    # print(label_encoder.classes_)
+
+    n_clusters = len(label_encoder.classes_)
+    preprocessor = Pipeline(
+        [
+            ("scaler", MinMaxScaler()),
+            ("pca", PCA(n_components=2, random_state=None)),
+        ]
+    )
+    clusterer = Pipeline(
+        [
+            (
+                "kmeans",
+                KMeans(
+                    n_clusters=n_clusters,
+                    init="k-means++",
+                    n_init=50,
+                    max_iter=500,
+                    random_state=42,
+                ),
+            ),
+        ]
+    )
+
+    pipe = Pipeline(
+        [
+            ("preprocessor", preprocessor),
+            ("clusterer", clusterer)
+        ]
+    )
+
+    pipe.fit(data)
+
+    preprocessed_data = pipe["preprocessor"].transform(data)
+
+    predicted_labels = pipe["clusterer"]["kmeans"].labels_
+
+    silhouette_score(preprocessed_data, predicted_labels)
+    adjusted_rand_score(true_labels, predicted_labels)
+
+    pcadf = pd.DataFrame(
+        pipe["preprocessor"].transform(data),
+        columns=["component_1","component_2"],
+    )
+
+    pcadf["predicted_cluster"] = pipe["clusterer"]["kmeans"].labels_
+    pcadf["true_label"] = label_encoder.inverse_transform(true_labels)
+
+    plt.style.use("fivethirtyeight")
+    plt.figure(figsize=(8, 8))
+
+    scat = sns.scatterplot(
+        "component_1",
+        "component_2",
+        s=50,
+        data=pcadf,
+        hue="predicted_cluster",
+        style="true_label",
+        palette="Set2",
+    )
+
+    scat.set_title(
+        "Resultados de clustering de piezas de Fur Elise"
+    )
+    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.0)
+
+    plt.show()
+
+
+def main():
+    le_file = "dataset.csv"
+    los_labels = 'labels.csv'
+
+    # ------ Extraccion con Pandas -------
+    features = pd.read_csv(r'dataset.csv')
+    features = pd.DataFrame(features)
+    # print(features)
+
+    data = np.genfromtxt(le_file, delimiter=",",
+                         usecols=range(1, 27), skip_header=1)
+
+    true_label_names = np.genfromtxt(los_labels, delimiter=",",
+                                     usecols=(1,), skip_header=1, dtype="str")
+
+    featuresFixed = features.iloc[:, 1:27]
+    # print(featuresFixed)
+
+    scaler = StandardScaler()
+    scaled_features = scaler.fit_transform(featuresFixed)
+    # print(scaled_features)
+
+    kmeansElaborado(data, true_label_names)
+
+
+if __name__ == "__main__":
+    main()
 
 # =========================================== CODIGO AUXILIAR ===========================================
-
-# data = pd.read_csv('dataset.csv')
-# data.head()  # Dropping unneccesary columns
-# data = data.drop(['filename'], axis=1)  # Encoding the Labels
-# genre_list = data.iloc[:, -1]
-# encoder = LabelEncoder()
-# y = encoder.fit_transform(genre_list)  # Scaling the Feature columns
-# scaler = StandardScaler()
-# # Dividing data into training and Testing set
-# X = scaler.fit_transform(np.array(data.iloc[:, :-1], dtype=float))
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-# model = Sequential()
-# model.add(layers.Dense(256, activation='relu',
-#                        input_shape=(X_train.shape[1],)))
-# model.add(layers.Dense(128, activation='relu'))
-# model.add(layers.Dense(64, activation='relu'))
-# model.add(layers.Dense(10, activation='softmax'))
-# model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-
-# classifier = model.fit(X_train,
-#                        y_train,
-#                        epochs=100,
-#                        batch_size=128)
