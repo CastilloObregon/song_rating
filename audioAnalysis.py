fd06fc4e60d3c09e877d06edc8204a6d8525f677
manupianista <manuelecastilloo@gmail.com>
2020-09-13 02:51:45 -0600

    audio feature extraction ya bonis

---
+++ audioAnalysis.py
@@ -0,0 +1,67 @@
+import librosa
+import librosa.display
+import IPython.display as ipd
+import matplotlib as matplotlib
+from matplotlib import pyplot as plt
+from pathlib import Path
+import sklearn
+import numpy as np
+import scipy
+import glob
+import os
+from numba import jit, cuda
+from numba import vectorize
+
+import pandas as pd
+
+from PIL import Image
+import pathlib
+import csv
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from keras import layers
+import keras
+from keras.models import Sequential
+import warnings
+
+from kneed import KneeLocator
+from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+
+df = pd.read_csv (r'dataset.csv') 
+print (df)
+
+
+# scaler = StandardScaler()
+# scaled_features = scaler.fit_transform(features)
+
+
+
+
+# =========================================== CODIGO AUXILIAR ===========================================
+
+# data = pd.read_csv('dataset.csv')
+# data.head()  # Dropping unneccesary columns
+# data = data.drop(['filename'], axis=1)  # Encoding the Labels
+# genre_list = data.iloc[:, -1]
+# encoder = LabelEncoder()
+# y = encoder.fit_transform(genre_list)  # Scaling the Feature columns
+# scaler = StandardScaler()
+# # Dividing data into training and Testing set
+# X = scaler.fit_transform(np.array(data.iloc[:, :-1], dtype=float))
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# model = Sequential()
+# model.add(layers.Dense(256, activation='relu',
+#                        input_shape=(X_train.shape[1],)))
+# model.add(layers.Dense(128, activation='relu'))
+# model.add(layers.Dense(64, activation='relu'))
+# model.add(layers.Dense(10, activation='softmax'))
+# model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# classifier = model.fit(X_train,
+#                        y_train,
+#                        epochs=100,
+#                        batch_size=128)
+
--- audio_featureExtraction.py
+++ audio_featureExtraction.py
@@ -1,6 +1,7 @@
 # ================== SCRIPT FOR MACOS ==================
 
-import librosa, librosa.display
+import librosa
+import librosa.display
 import IPython.display as ipd
 import matplotlib as matplotlib
 from matplotlib import pyplot as plt
@@ -8,9 +9,22 @@ from pathlib import Path
 import sklearn
 import numpy as np
 import scipy
-import glob, os
+import glob
+import os
 from numba import jit, cuda
 from numba import vectorize
+
+import pandas as pd
+
+from PIL import Image
+import pathlib
+import csv
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from keras import layers
+import keras
+from keras.models import Sequential
+import warnings
 
 audios = []
 listaAudios = []
@@ -25,7 +39,8 @@ posDatas = []
 
 noDir = 'Audios/'
 
-#os.chdir("Audios")
+# os.chdir("Audios")
+
 
 def audioDatabase():
     for file in glob.glob("Audios/*.wav"):
@@ -33,7 +48,6 @@ def audioDatabase():
         # print(file)
 
     print("Los audios son: ", audios)
-
 
 
 # ================================== FUNCIÓN PARA EXTRAER CARACTERÍSTICAS DEL AUDIO ===========================
@@ -49,27 +63,29 @@ def audioFeatures(audioFile):
     # print(sr)
     # x = np.linspace(0, 2 * np.pi, 400)
     # y = np.sin(x ** 2)
-    
+
     return data, sr, dataRms, rmsShape, dataShape, ind
+
 
 def listaLimpia():
     for aud in audios:  # iterating on a copy since removing will mess things up
         new_string = aud.replace(noDir, "")
         listaAudios.append(new_string)
-    print("Hola somos la lista simplificada: ",listaAudios)
+    print("Hola somos la lista simplificada: ", listaAudios)
     # print("Hola somos la lista normal: ",audios)
-    
+
 
 # ================================== FUNCIÓN PARA AGREGAR CARACTERÍSTICAS DEL AUDIO A LISTA ===========================
 
 def appenDataAndSr():
     for lesAudios in audios:
-        laData, elSr, dataRms, rmsShape, daShape, splitIndex = audioFeatures(lesAudios)
-        
+        laData, elSr, dataRms, rmsShape, daShape, splitIndex = audioFeatures(
+            lesAudios)
+
         # laPreData1 = librosa.effects.trim(laData)
         # S = librosa.magphase(librosa.stft(laData, window=np.ones, center=False))[0]
         # laPreData2 = librosa.feature.rms(y=laData)
-        
+
         datas.append(laData)
         posDatas.append(dataRms)
         srs.append(elSr)
@@ -80,6 +96,7 @@ def appenDataAndSr():
     print("Hola, somos posDatas: ", posDatas)
     print("Hola, somos srs: ", srs)
 
+
 def audioLen(audioName, theData, theSr):
     durAudio = librosa.get_duration(theData, theSr)
     texto = audioName + " dura: "+str(durAudio)
@@ -87,17 +104,20 @@ def audioLen(audioName, theData, theSr):
 
 # ================================== FUNCIONES PARA MOSTRAR WAVEPLOTS Y ESPECTOGRAMAS ===========================
 
+
 def saveWaveplots(audioName, theData, theSr):
     # plt.figure(figsize=(10, 4))
     # forPiano = librosa.effects.split(theData, frame_length=100, hop_length=50)
     librosa.display.waveplot(theData, sr=theSr)
     # plt.show()
-    filename = 'Images/Waveplots/' + str(audioName) +'.png'
+    filename = 'Images/Waveplots/' + str(audioName) + '.png'
     # files.append(filename)
     plt.savefig(filename)
     plt.clf()
 
-# @jit(target ="cuda") 
+# @jit(target ="cuda")
+
+
 def saveSpectograms(audioName, theData, theSr):
     X = librosa.stft(theData)
     Xdb = librosa.amplitude_to_db(abs(X))
@@ -105,18 +125,20 @@ def saveSpectograms(audioName, theData, theSr):
     librosa.display.specshow(Xdb, sr=theSr, x_axis='time', y_axis='hz')
     # plt.colorbar()
     # plt.show()
-    filename = 'Images/Espectogramas/Hz/' + str(audioName) +'.png'
+    filename = 'Images/Espectogramas/Hz/' + str(audioName) + '.png'
     plt.savefig(filename)
     librosa.display.specshow(Xdb, sr=theSr, x_axis='time', y_axis='log')
     # plt.show()
-    filename2 = 'Images/Espectogramas/Log/' + str(audioName) +'.png'
+    filename2 = 'Images/Espectogramas/Log/' + str(audioName) + '.png'
     plt.savefig(filename2)
 
-# @jit(target ="cuda") 
+# @jit(target ="cuda")
+
+
 def melSpectograms(audioName, theData, theSr):
 
     pianosong, trIndex = librosa.effects.trim(theData)
-    
+
     # forPiano, forTrIndex = librosa.effects.split(pianosong)
     # Transformada de Fourier
     # === Short time fourier transform ===
@@ -135,14 +157,35 @@ def melSpectograms(audioName, theData, theSr):
 
     # Espectograma de Mel
     DB = librosa.amplitude_to_db(D, ref=np.max)
-    
+
     librosa.display.specshow(
         DB, sr=theSr, hop_length=hop_length, x_axis='time', y_axis='log')
     # plt.colorbar(format='%+2.0f dB')
     # plt.show()
-    filename2 = 'Images/Mel_Spectograms/y_axis_mel/' + str(audioName) +'.png'
+    filename2 = 'Images/Mel_Spectograms/y_axis_mel/' + str(audioName) + '.png'
     plt.savefig(filename2)
     plt.clf()
+
+
+def createCsv(audioName, theData, theSr):
+    filename = audioName
+    y = theData
+    sr = theSr
+    rmse = librosa.feature.rms(y=y)
+    chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
+    spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
+    spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
+    rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
+    zcr = librosa.feature.zero_crossing_rate(y)
+    mfcc = librosa.feature.mfcc(y=y, sr=sr)
+    to_append = f'{filename} {np.mean(chroma_stft)} {np.mean(rmse)} {np.mean(spec_cent)} {np.mean(spec_bw)} {np.mean(rolloff)} {np.mean(zcr)}'
+    for e in mfcc:
+        to_append += f' {np.mean(e)}'
+
+    file = open('dataset.csv', 'a', newline='')
+    with file:
+        writer = csv.writer(file)
+        writer.writerow(to_append.split())
 
 
 def main():
@@ -152,12 +195,12 @@ def main():
     listaLimpia()
 
     for losAudios, datos, senales in zip(listaAudios, posDatas, srs):
-        audioLen(losAudios, datos,senales)
+        audioLen(losAudios, datos, senales)
     print("La duracion de los audios es: ")
     print(audiosLen)
 
     for losAudios, datos, senales in zip(listaAudios, posDatas, srs):
-        saveWaveplots(losAudios, datos,senales)
+        saveWaveplots(losAudios, datos, senales)
     print("Waveplots generados")
     # print("Generando Espectogramas")
     # for losAudios, datos, senales in zip(listaAudios, datas, srs):
@@ -168,10 +211,24 @@ def main():
         melSpectograms(losAudios, datos, senales)
     print("Espectogramas tipo mel generados")
 
+    print("Generando CSV ...")
+
+    header = 'filename chroma_stft rmse spectral_centroid spectral_bandwidth rolloff zero_crossing_rate'
+    for i in range(1, 21):
+        header += f' mfcc{i}'
+    header += ' label'
+    header = header.split()
+
+    file = open('dataset.csv', 'w', newline='')
+    with file:
+        writer = csv.writer(file)
+        writer.writerow(header)
+    for losAudios, datos, senales in zip(listaAudios, posDatas, srs):
+        createCsv(losAudios, datos, senales)
+
+
 if __name__ == "__main__":
     main()
-
-
 
 
 # ================================== CÓDIGO AUXILIAR ===========================
@@ -190,6 +247,6 @@ if __name__ == "__main__":
 # for i, x in enumerate(piano_signals):
 #     plt.subplot(2, 2, i+1)
 #     librosa.display.waveplot(x[:1000000])
-    
+
 
 # plt.show()
